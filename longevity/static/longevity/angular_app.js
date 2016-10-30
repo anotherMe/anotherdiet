5f7ec11d28afc55f6f49b3f349ffccde480ca0c6
Bernie Lomax <bernie@debian>
2016-10-30 20:31:56 +0100

    Added ngResource. Modified the base page layout.

--- longevity/static/longevity/angular_app.js
+++ longevity/static/longevity/angular_app.js
@@ -1,17 +1,24 @@
 (function(){
 
-    var foo = {
-        name: "Foo\'n\'tastic",
-        canSpin: true,
-        canRoll: false
-    };
+    var app = angular.module('longevity_diet', ['ngResource']);
 
-    var app = angular.module('longevity_diet', []);
-
-    app.controller('FooController', function() {
-        
-        this.foo = foo;
-        
+    app.config(['$resourceProvider', function($resourceProvider) {
+        // Don't strip trailing slashes from calculated URLs
+        $resourceProvider.defaults.stripTrailingSlashes = false;
+    }]);
+    
+    app.factory("Alimento", function($resource) {
+        return $resource("/longevity/api/alimento/:id");
     });
+
+    app.controller('MainController', ['$http', 'Alimento', function($http, Alimento) {
+
+        var t = this;
+
+        Alimento.get({ id: 1 }, function(data) {
+            t.foo = data;
+        });
+        
+    }]);
 
 })();
