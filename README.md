3fbf7572b4f949e00ef73e8c0c3cc4a56bf42f27
marco <tuplanulla@gmail.com>
2016-11-25 19:23:34 +0100

    Renamed project to "thepot". Added PyCharm project folder (".idea").

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# anotherdiet
+# thepot
 
 ## Memo
 
--- thepot/wsgi.py
+++ thepot/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for anotherdiet project.
+WSGI config for thepot project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anotherdiet.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "thepot.settings")
 
 application = get_wsgi_application()
